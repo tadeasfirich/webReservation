d8df1079387c5f6ff05010eacb8c0a4354c7c135
Tadeas Firich <tadeasfirich696@gmail.com>
2017-12-12 13:26:26 +0100

    make some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,10 +24,52 @@ def room_code
   FFaker::Color.hex_code
 end
 
-10.times {
-  Building.create!(:title => building_name, :code => building_code)
-}
-10.times {
-  r = rand * (11-1) + 1
-  Room.create!(:title => room_name, :code => room_code, :building => Building.find(r.to_i))
-}
+def first_name
+  FFaker::NameCS.first_name
+end
+
+def last_name
+  FFaker::NameCS.last_name
+end
+
+def email
+  FFaker::Internet.email
+end
+
+def course_title
+  FFaker::Food.fruit
+end
+
+
+
+# #Buildings
+# 10.times {
+#   Building.create!(:title => building_name, :code => building_code)
+# }
+#
+# #Rooms
+# 10.times {
+#   r = rand * (11-1) + 1
+#   Room.create!(:title => room_name, :code => room_code, :building => Building.find(r.to_i))
+# }
+#
+# #Teacher
+# 10.times {
+#   Teacher.create!(:first_name => first_name, :last_name => last_name, :email => email)
+# }
+#
+# #Courses
+# #TODO: dodělat language a study_type
+# 10.times {
+#   r = rand * (2-1) + 1
+#   s = rand * (2-1) + 1
+#   Building.create!(:title => course_title, :code => building_code, :language => 'CZECH', :ENGLISH => 'PART_TIME')
+# }
+#
+# #Students
+# 10.times {
+#   r = rand * (2-1) + 1
+#   Building.create!(:title => room_name, :code => building_code)
+# }
+
+puts Building.attribute_names
