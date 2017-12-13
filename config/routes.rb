259c903ec474152de164eebb6950a33b337c2866
Tadeas Firich <tadeasfirich696@gmail.com>
2017-12-13 16:50:02 +0100

    add building/room:id

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resources :courses
   resources :teachers
   resources :rooms
+  get '/buildings/:id', to: 'buildings#show', as: 'building'
   resources :buildings
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
