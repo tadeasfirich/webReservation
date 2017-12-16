8037d1ec08a089f60a048667204e545724c4e1fe
Tadeas Firich <tadeasfirich696@gmail.com>
2017-12-16 13:54:59 +0100

    solve TODO in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
   root to: 'buildings#index'
 
-  #TODO: předělat routy nefunguje kalendar po stisknutí na room
-  resources :rooms do
-    resources :rooms, only: [:show, :index]
-    resources :lessons, only: [:index, :show]
-  end
-
+  resources :rooms, only: [:show, :index]
   get '/buildings/:id/today', to: 'buildings#show_today', as: 'building_today'
   resources :buildings, only: [:show, :index]
-  # get '/buildings', to: 'buildings#index', as: 'buildings'
-  # get '/buildings/:id', to: 'buildings#show', as: 'building'
-  # get '/buildings/:id/today', to: 'buildings#show_today', as: 'building_today'
-  # get '/rooms', to: 'rooms#index', as: 'rooms'
-  # get '/rooms/:id', to: 'rooms#show', as: 'room'
-
+  
   namespace :admin do
     resources :student_assigments
     resources :teacher_assigments
